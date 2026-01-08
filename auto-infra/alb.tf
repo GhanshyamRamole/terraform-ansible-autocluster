4850b71293132dc9e971bb1e5dde17054ae7c471
Ubuntu <ubuntu@ip-172-31-29-223.us-west-1.compute.internal>
2026-01-08 06:24:32 +0000

    error-free

--- auto-infra/alb.tf
+++ auto-infra/alb.tf
@@ -3,6 +3,7 @@ resource "aws_lb" "alb" {
   load_balancer_type = "application"
   subnets            = var.subnets
   security_groups    = [aws_security_group.common.id]
+  tags               = var.tags
 }
 
 resource "aws_lb_target_group" "tg" {
@@ -10,6 +11,10 @@ resource "aws_lb_target_group" "tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  health_check {
+    path = "/"
+    port = 80
+  }
 }
 
 resource "aws_lb_target_group_attachment" "attach" {
@@ -29,4 +34,3 @@ resource "aws_lb_listener" "listener" {
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
-
