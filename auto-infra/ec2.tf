c6b027cf861a57799d2d12aec4da28d954de95af
Ubuntu <ubuntu@ip-172-31-29-223.us-west-1.compute.internal>
2026-01-08 08:27:48 +0000

    logic for data_user

--- auto-infra/ec2.tf
+++ auto-infra/ec2.tf
@@ -8,23 +8,6 @@ resource "aws_key_pair" "key" {
   public_key = file(var.public_key_path)
 }
 
-resource "aws_instance" "master" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = aws_key_pair.key.key_name
-  
-  subnet_id              = var.subnets[0]
-  vpc_security_group_ids = [aws_security_group.common.id]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y ansible python3 git
-  EOF
-
-  tags = merge(var.tags, { Name = "ansible-master" })
-}
-
 resource "aws_instance" "workers" {
   count         = var.worker_count
   ami           = var.ami
@@ -34,5 +17,34 @@ resource "aws_instance" "workers" {
   subnet_id              = element(var.subnets, count.index)
   vpc_security_group_ids = [aws_security_group.common.id]
 
+  user_data = <<-EOF
+  #!/bin/bash
+  useradd itsadmin
+  echo 111 | passwd --stdin itsadmin
+  echo 111 | passwd --stdin root
+  echo "itsadmin  ALL=(ALL)   NOPASSWD: ALL" >> /etc/sudoers
+  sed 's/PasswordAuthentication no/PasswordAuthentication yes/' -i /etc/ssh/sshd_config
+  echo PermitRootLogin yes >> /etc/ssh/sshd_config
+  systemctl restart sshd
+  EOF
+
   tags = merge(var.tags, { Name = "worker-${count.index + 1}" })
 }
+   
+
+
+resource "aws_instance" "master" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.key.key_name
+  
+  subnet_id              = var.subnets[0]
+  vpc_security_group_ids = [aws_security_group.common.id]
+  
+  user_data = templatefile("${path.module}/user_data.tftpl", {
+    worker_ips = aws_instance.workers[*].private_ip
+  })
+ 
+  tags = merge(var.tags, { Name = "ansible-master" })
+}
+
