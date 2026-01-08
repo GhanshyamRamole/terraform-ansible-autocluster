26e79ae864619241e7a3232367d9df4505a85654
Ubuntu <ubuntu@ip-172-31-29-223.us-west-1.compute.internal>
2026-01-08 06:29:33 +0000

    inventory_creation file edited

--- auto-infra/inventory_creation.tf
+++ auto-infra/inventory_creation.tf
@@ -1,8 +1,8 @@
 resource "local_file" "inventory" {
   content = templatefile("${path.module}/inventory.tpl", {
-    master_ip = aws_instance.master.public_ip
+    master_ip  = aws_instance.master.public_ip
     worker_ips = aws_instance.workers[*].public_ip
   })
-  filename = "../ansible/inventory.ini"
+  # IMPORTANT: Ensure the '../ansible' directory exists before running apply
+  filename = "${path.module}/../ansible/inventory.ini"
 }
-
