87350750de147f0c9345639481bedfbb0a6fd046
Ubuntu <ubuntu@ip-172-31-29-223.us-west-1.compute.internal>
2026-01-08 06:21:38 +0000

    update

--- auto-infra/ec2.tf
+++ auto-infra/ec2.tf
@@ -4,15 +4,17 @@
 	###########################################################
 
 resource "aws_key_pair" "key" {
-  key_name   = "terraform-key"
+  key_name   = "terraform-key-new"        # Changed name to avoid conflicts if recreating
   public_key = file(var.public_key_path)
 }
 
 resource "aws_instance" "master" {
   ami           = var.ami
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   key_name      = aws_key_pair.key.key_name
-  security_groups = [aws_security_group.common.name]
+  
+  subnet_id              = var.subnets[0]
+  vpc_security_group_ids = [aws_security_group.common.id]
 
   user_data = <<-EOF
     #!/bin/bash
@@ -20,20 +22,17 @@ resource "aws_instance" "master" {
     yum install -y ansible python3 git
   EOF
 
-  tags = {
-    Name = "ansible-master"
-  }
+  tags = merge(var.tags, { Name = "ansible-master" })
 }
 
 resource "aws_instance" "workers" {
   count         = var.worker_count
   ami           = var.ami
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   key_name      = aws_key_pair.key.key_name
-  security_groups = [aws_security_group.common.name]
 
-  tags = {
-    Name = "worker-${count.index}"
-  }
+  subnet_id              = element(var.subnets, count.index)
+  vpc_security_group_ids = [aws_security_group.common.id]
+
+  tags = merge(var.tags, { Name = "worker-${count.index + 1}" })
 }
-
